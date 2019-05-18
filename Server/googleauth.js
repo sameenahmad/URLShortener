358d6771022ccb7af96129314df54054e8602dcf
Sameen Ahmad <sameen.ahmad21@gmail.com>
2019-05-19 01:52:05 +0530

    Updated ClientID

--- Server/googleauth.js
+++ Server/googleauth.js
@@ -5,9 +5,9 @@ const client = new OAuth2Client(
 async function verify() {
   const ticket = await client.verifyIdToken({
     idToken: token,
-    audience:
-      [153994507565 -
-      r7bljvm8rpc365af886ghcm7fb2bb91o.apps.googleusercontent.com] // Specify the CLIENT_ID of the app that accesses the backend
+    audience: [
+      153994507565 - r7bljvm8rpc365af886ghcm7fb2bb91o.apps.googleusercontent.com
+    ] // Specify the CLIENT_ID of the app that accesses the backend
     // Or, if multiple clients access the backend:
     //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
   });
--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,3 @@
-
 //Get User ID
 gapi.load("auth2", function() {
   auth2 = gapi.auth2.init({
@@ -7,38 +6,25 @@ gapi.load("auth2", function() {
     fetch_basic_profile: false,
     scope: "profile"
   });
-// Send token to backend
-function onSignIn(googleUser) {
-  var id_token = googleUser.getAuthResponse().id_token;
-}
-var url = "http://localhost:5000/api/url";
-var formData = new FormData();
-    formData.append({idToken: id_token});
-    var request = {
-    method: 'POST',
+  // Send token to backend
+  function onSignIn(googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+  }
+  var url = "http://localhost:5000/api/url";
+  var formData = new FormData();
+  formData.append({ idToken: id_token });
+  var request = {
+    method: "POST",
     headers: headers,
     body: formData
-};
-fetch(url, request);
-
-
-
-
-
-
-
-
-
-
+  };
+  fetch(url, request);
 
   // Sign the user in, and then retrieve their ID.
   auth2.signIn().then(function() {
     console.log(auth2.currentUser.get().getId());
   });
 });
-
-
-
 
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
